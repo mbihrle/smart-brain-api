33e786e41bbda78b2bccc6c1fbb5937610e70336
Marco Bihrle <bihrle.marco@hotmail.com>
2022-02-03 11:27:00 +0100

    initial update

--- server.js
+++ server.js
@@ -1,37 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+process.title = "HTTP Server";
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+
+require("dotenv").config({
+    path: __dirname + "./../../../.configs/smart-brain.env",
+});
+
+// console.log(process.env);
+const express = require("express");
+// const bodyParser = require("body-parser"); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 
 const db = knex({
-  // connect to your own database here:
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    // connect to your own database here:
+    client: "pg",
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB,
+    },
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+    res.send(db.users);
+});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) => {
+    register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+    profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+    image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+    image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+app.listen(3000, () => {
+    console.log("app is running on port 3000");
+});
